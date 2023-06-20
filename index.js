49cb8d6b6dc4c8cbf1179b4f011577b46b6b18fa
arnoldroshan75 <33338807+arnoldroshan75@users.noreply.github.com>
2023-06-20 15:58:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,104 +1,87 @@
-var makeMiddleware = require('./lib/make-middleware')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const createAdapter = require("@socket.io/redis-adapter").createAdapter;
+const redis = require("redis");
+require("dotenv").config();
+const { createClient } = redis;
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
-var diskStorage = require('./storage/disk')
-var memoryStorage = require('./storage/memory')
-var MulterError = require('./lib/multer-error')
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-function allowAll (req, file, cb) {
-  cb(null, true)
-}
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-function Multer (options) {
-  if (options.storage) {
-    this.storage = options.storage
-  } else if (options.dest) {
-    this.storage = diskStorage({ destination: options.dest })
-  } else {
-    this.storage = memoryStorage()
-  }
+const botName = "ChatCord Bot";
 
-  this.limits = options.limits
-  this.preservePath = options.preservePath
-  this.fileFilter = options.fileFilter || allowAll
-}
+(async () => {
+  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+  await pubClient.connect();
+  subClient = pubClient.duplicate();
+  io.adapter(createAdapter(pubClient, subClient));
+})();
 
-Multer.prototype._makeMiddleware = function (fields, fileStrategy) {
-  function setup () {
-    var fileFilter = this.fileFilter
-    var filesLeft = Object.create(null)
+// Run when client connects
+io.on("connection", (socket) => {
+  console.log(io.of("/").adapter);
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-    fields.forEach(function (field) {
-      if (typeof field.maxCount === 'number') {
-        filesLeft[field.name] = field.maxCount
-      } else {
-        filesLeft[field.name] = Infinity
-      }
-    })
+    socket.join(user.room);
 
-    function wrappedFileFilter (req, file, cb) {
-      if ((filesLeft[file.fieldname] || 0) <= 0) {
-        return cb(new MulterError('LIMIT_UNEXPECTED_FILE', file.fieldname))
-      }
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
 
-      filesLeft[file.fieldname] -= 1
-      fileFilter(req, file, cb)
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
     }
+  });
+});
 
-    return {
-      limits: this.limits,
-      preservePath: this.preservePath,
-      storage: this.storage,
-      fileFilter: wrappedFileFilter,
-      fileStrategy: fileStrategy
-    }
-  }
+const PORT = process.env.PORT || 3000;
 
-  return makeMiddleware(setup.bind(this))
-}
-
-Multer.prototype.single = function (name) {
-  return this._makeMiddleware([{ name: name, maxCount: 1 }], 'VALUE')
-}
-
-Multer.prototype.array = function (name, maxCount) {
-  return this._makeMiddleware([{ name: name, maxCount: maxCount }], 'ARRAY')
-}
-
-Multer.prototype.fields = function (fields) {
-  return this._makeMiddleware(fields, 'OBJECT')
-}
-
-Multer.prototype.none = function () {
-  return this._makeMiddleware([], 'NONE')
-}
-
-Multer.prototype.any = function () {
-  function setup () {
-    return {
-      limits: this.limits,
-      preservePath: this.preservePath,
-      storage: this.storage,
-      fileFilter: this.fileFilter,
-      fileStrategy: 'ARRAY'
-    }
-  }
-
-  return makeMiddleware(setup.bind(this))
-}
-
-function multer (options) {
-  if (options === undefined) {
-    return new Multer({})
-  }
-
-  if (typeof options === 'object' && options !== null) {
-    return new Multer(options)
-  }
-
-  throw new TypeError('Expected object for argument options')
-}
-
-module.exports = multer
-module.exports.diskStorage = diskStorage
-module.exports.memoryStorage = memoryStorage
-module.exports.MulterError = MulterError
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
