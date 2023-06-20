34950cf171d4e11aae458fc33cd42bbea81480e6
arnoldroshan75 <33338807+arnoldroshan75@users.noreply.github.com>
2023-06-20 15:35:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,155 +1,148 @@
-'use strict';
-
-const majorNodeVersion = process.versions.node.split('.')[0];
-
-if (typeof global.gc !== 'function') {
-  // Construct the correct (version-dependent) command-line args.
-  const args = ['--expose-gc'];
-  const majorV8Version = process.versions.v8.split('.')[0];
-  if (majorV8Version < 9) {
-    args.push('--no-concurrent-array-buffer-freeing');
+{
+  "name": "express-image-recognition",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "dev": "nodemon server.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "@tensorflow/tfjs": "^4.2.0",
+    "express": "^4.18.2",
+    "multer": "^1.4.5-lts.1",
+    "nodemon": "^2.0.22"
   }
-  if (majorNodeVersion >= 14) {
-    args.push('--no-concurrent-array-buffer-sweeping');
-  }
-  args.push(__filename);
-
-  const child = require('./napi_child').spawnSync(process.argv[0], args, {
-    stdio: 'inherit'
-  });
-
-  if (child.signal) {
-    console.error(`Tests aborted with ${child.signal}`);
-    process.exitCode = 1;
-  } else {
-    process.exitCode = child.status;
-  }
-  process.exit(process.exitCode);
 }
+// Importing necessary packages
+const express = require('express');
+const multer = require('multer');
+const tf = require('@tensorflow/tfjs-node');
+const sharp = require('sharp');
+const predictImageContents = require('./TensorFlow')
 
-const testModules = [];
+// Creating an Express application
+const app = express();
 
-const fs = require('fs');
-const path = require('path');
+// Creating a multer instance with memory storage
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
-let filterCondition = process.env.npm_config_filter || '';
-let filterConditionFiles = [];
-
-if (filterCondition !== '') {
-  filterCondition = require('../unit-test/matchModules').matchWildCards(process.env.npm_config_filter);
-  filterConditionFiles = filterCondition.split(' ').length > 0 ? filterCondition.split(' ') : [filterCondition];
-}
-
-const filterConditionsProvided = filterConditionFiles.length > 0;
-
-function checkFilterCondition (fileName, parsedFilepath) {
-  let result = false;
-
-  if (!filterConditionsProvided) return true;
-  if (filterConditionFiles.includes(parsedFilepath)) result = true;
-  if (filterConditionFiles.includes(fileName)) result = true;
-  return result;
-}
-
-// TODO(RaisinTen): Update this when the test filenames
-// are changed into test_*.js.
-function loadTestModules (currentDirectory = __dirname, pre = '') {
-  fs.readdirSync(currentDirectory).forEach((file) => {
-    if (currentDirectory === __dirname && (
-      file === 'binding.cc' ||
-          file === 'binding.gyp' ||
-          file === 'build' ||
-          file === 'common' ||
-          file === 'napi_child.js' ||
-          file === 'testUtil.js' ||
-          file === 'thunking_manual.cc' ||
-          file === 'thunking_manual.js' ||
-          file === 'index.js' ||
-          file[0] === '.')) {
-      return;
+// POST route for uploading an image and predicting its contents
+app.post('/image', upload.single('image'), async (req, res) => {
+    const buffer = req.file.buffer;
+    const decodedImage = tf.node.decodeImage(buffer);
+    // Checking if the image type is supported
+    if (!/^image\/(jpe?g|png|gif)$/i.test(req.file.mimetype)) {
+        console.log('Unsupported image type');
+        res.json({ error: "Unsupported image type" })
+        return;
     }
-    const absoluteFilepath = path.join(currentDirectory, file);
-    const parsedFilepath = path.parse(file);
-    const parsedPath = path.parse(currentDirectory);
+    // Resizing the image to 224x224 and converting it to a buffer
+    const resizedImage = await sharp(req.file.buffer)
+        .resize(224, 224)
+        .toBuffer();
 
-    if (fs.statSync(absoluteFilepath).isDirectory()) {
-      if (fs.existsSync(absoluteFilepath + '/index.js')) {
-        if (checkFilterCondition(parsedFilepath.name, parsedPath.base)) {
-          testModules.push(pre + file);
-        }
-      } else {
-        loadTestModules(absoluteFilepath, pre + file + '/');
-      }
-    } else {
-      if (parsedFilepath.ext === '.js' && checkFilterCondition(parsedFilepath.name, parsedPath.base)) {
-        testModules.push(pre + parsedFilepath.name);
-      }
-    }
-  });
-}
+    // Predicting the image contents using the TensorFlow model
+    const result = await predictImageContents(resizedImage);
 
-loadTestModules();
-
-let napiVersion = Number(process.versions.napi);
-if (process.env.NAPI_VERSION) {
-  // we need this so that we don't try run tests that rely
-  // on methods that are not available in the NAPI_VERSION
-  // specified
-  napiVersion = process.env.NAPI_VERSION;
-}
-console.log('napiVersion:' + napiVersion);
-
-if (napiVersion < 3) {
-  testModules.splice(testModules.indexOf('env_cleanup'), 1);
-  testModules.splice(testModules.indexOf('callbackscope'), 1);
-  testModules.splice(testModules.indexOf('version_management'), 1);
-}
-
-if (napiVersion < 4 && !filterConditionsProvided) {
-  testModules.splice(testModules.indexOf('asyncprogressqueueworker'), 1);
-  testModules.splice(testModules.indexOf('asyncprogressworker'), 1);
-  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_ctx'), 1);
-  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_existing_tsfn'), 1);
-  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_ptr'), 1);
-  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_sum'), 1);
-  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function_unref'), 1);
-  testModules.splice(testModules.indexOf('threadsafe_function/threadsafe_function'), 1);
-}
-
-if (napiVersion < 5 && !filterConditionsProvided) {
-  testModules.splice(testModules.indexOf('date'), 1);
-}
-
-if (napiVersion < 6 && !filterConditionsProvided) {
-  testModules.splice(testModules.indexOf('addon'), 1);
-  testModules.splice(testModules.indexOf('addon_data'), 1);
-  testModules.splice(testModules.indexOf('bigint'), 1);
-  testModules.splice(testModules.indexOf('typedarray-bigint'), 1);
-}
-
-if (majorNodeVersion < 12 && !filterConditionsProvided) {
-  testModules.splice(testModules.indexOf('objectwrap_worker_thread'), 1);
-  testModules.splice(testModules.indexOf('error_terminating_environment'), 1);
-}
-
-if (napiVersion < 8 && !filterConditionsProvided) {
-  testModules.splice(testModules.indexOf('object/object_freeze_seal'), 1);
-  testModules.splice(testModules.indexOf('type_taggable'), 1);
-}
-
-(async function () {
-  console.log(`Testing with Node-API Version '${napiVersion}'.`);
-
-  if (filterConditionsProvided) { console.log('Starting test suite\n', testModules); } else { console.log('Starting test suite\n'); }
-
-  // Requiring each module runs tests in the module.
-  for (const name of testModules) {
-    console.log(`Running test '${name}'`);
-    await require('./' + name);
-  }
-
-  console.log('\nAll tests passed!');
-})().catch((error) => {
-  console.log(error);
-  process.exit(1);
+    // Sending the predicted result back as a response
+    res.send(result);
 });
+
+// Setting the view engine to ejs
+app.set('view engine', 'ejs');
+
+// GET route for rendering the index page
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+// Starting the Express server on port 3000
+app.listen(3000, () => {
+    console.log('Server listening on port 3000!');
+});
+const tf = require('@tensorflow/tfjs-node');
+const classes = require('./class_name');
+
+const TOP_K = 1; // Number of top predictions to return
+async function predictImageContents(imageData) {
+    const model = await tf.loadLayersModel(
+        "https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json"
+    );
+    const uint8Array = new Uint8Array(imageData.buffer);
+    const decodedImage = tf.node.decodeImage(uint8Array);
+    const resizedImage = tf.image.resizeBilinear(decodedImage, [224, 224]);
+    const normalizedImage = resizedImage.div(255.0).expandDims();
+
+    // Make the prediction
+    const prediction = model.predict(normalizedImage);
+
+    // Get the top k predictions
+    const topK = await prediction.topk(TOP_K);
+
+    // Get the predicted class and label
+    const classIndex = topK.indices.dataSync()[0];
+    const label = classes[classIndex];
+
+    // Return the result
+    return { classIndex, label };
+}
+
+module.exports = predictImageContents;
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sentayhu's Image Recognition</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Image recogntion System</h1>
+<p>Welcome to image recognition system </p>
+<form>
+  <input type="file" id="image">
+  <button type="submit" id="submit">Submit</button>
+</form>
+<h2 id="info"></h2>
+</body>
+</html>
+<script>
+// Get the HTML elements
+const form = document.querySelector('form');
+const imageInput = document.querySelector('#image');
+const submitButton = document.querySelector('#submit');
+const info = document.querySelector('#info');
+const url = "http://localhost:3000/image";
+
+// Add event listener to submit button
+submitButton.addEventListener('click', async (event) => {
+  // Show loading message
+  info.textContent = "Loading...";
+  event.preventDefault();
+  const formData = new FormData();
+  formData.append('image', imageInput.files[0]);
+  
+  // Send POST request to the server
+  const response = await fetch(url, {
+    method: 'POST',
+    body: formData
+  });
+  
+  // Get the prediction result
+  const result = await response.json();
+  console.log(result);
+ 
+  // Update the UI with the predicted label
+  info.textContent = result.label;
+});
+</script>
+
+
+
